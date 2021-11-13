897658b990add616ec4d9dd49e03fd0bed1b9398
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-11-13 20:08:41 -0300

    update main

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main'
 
-function App() {
+const App = () => {
   return (
     <Main />
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import './Main.css';
 
-const Main = () => {
-    return(
-        <h1>Ola Mundo !</h1>
-    );
+export default class Main extends Component {
+    state = {
+        novaTarefa: '',
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            novaTarefa: e.target.value,
+        })
+    }
+
+    render() {
+       // const { novaTarefa } = this.state;
+        return (
+            <div className="main">
+                <h1>Lista de tarefas</h1>
+
+                <form action="#">
+                    <input onChange={this.handleChange} type="text" />
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+        );
+    }
 }
-
-export default Main;
