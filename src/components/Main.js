84ccbb732b93c10a357ee8667009a657f5abf102
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-11-13 20:20:36 -0300

    update style form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import './Main.css';
 
+// Form
+import {FaPlus} from  'react-icons/fa'
+
 export default class Main extends Component {
     state = {
         novaTarefa: '',
@@ -13,14 +16,21 @@ export default class Main extends Component {
     }
 
     render() {
-       // const { novaTarefa } = this.state;
+        const { novaTarefa } = this.state;
         return (
             <div className="main">
                 <h1>Lista de tarefas</h1>
 
-                <form action="#">
-                    <input onChange={this.handleChange} type="text" />
-                    <button type="submit">Enviar</button>
+                <form action="#" className="form">
+                    <input
+                        onChange = {this.handleChange}
+                        type = "text"
+                        value = {novaTarefa}
+                    />
+
+                    <button type="submit">
+                        <FaPlus />
+                    </button>
                 </form>
             </div>
         );
